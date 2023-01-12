5c7700123079b3809e87005fd71b7a827f3f1d87
Piotr Starzec <piotr_starzec@epam.com>
2023-01-12 11:08:01 +0100

    add angular material

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     ReactiveFormsModule,
   ],
--- ui/src/app/dashboard/dashboard.module.ts
+++ ui/src/app/dashboard/dashboard.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from 'src/app/dashboard/dashboard-routing.module';
 import { DashboardComponent } from 'src/app/dashboard/dashboard.component';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   declarations: [
     DashboardComponent
   ],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        DashboardRoutingModule,
+        MatButtonModule,
+    ],
 })
 export class DashboardModule { }
